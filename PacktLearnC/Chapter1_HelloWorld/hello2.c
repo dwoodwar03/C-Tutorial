69898799df46de11be602fefa10ae1139da2591e
Dave Woodward <dave@woodwardmail.com>
2023-11-05 14:44:29 +0000

    Chapter 1 - C File with Comments

---
+++ PacktLearnC/Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,16 @@
+/*
+ * hello2.c
+ * My first C program with comments.
+ * by D Woodward
+ * created 05 Nov 2023
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
+
+/* eof */
